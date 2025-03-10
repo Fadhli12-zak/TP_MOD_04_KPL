f2d856d68a85d93966b420afdb9d33341a6da78e
fadhlidzaki <fadhlidzaki120@gmail.com>
2025-03-10 11:14:54 +0700

    menambahkan implementasi dengan table driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class MainProgram
+{
+    public static void Main(String[] args)
+    {
+        KodePos kodepos = new KodePos();
+        Console.WriteLine("Masukkan nama kelurahan: ");
+        string input = Console.ReadLine();
+        String search = kodepos.getKodePos(input);
+        if(search == null)
+        {
+            Console.WriteLine("Kelurahan tidak ditemukan");
+        }
+        else
+        {
+            Console.WriteLine($"Kode Pos {input} adalah {search}");
+        }
+    }
+}
+
+ class KodePos
+{
+    String[] kelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+    String[] kodepos = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", "40272", "40274", "40273" };
+
+    public string getKodePos(string kelurahanTarget)
+    {
+       for (int i = 0; i < kelurahan.Length; i++)
+        {
+            if (kelurahan[i].Equals(kelurahanTarget, StringComparison.OrdinalIgnoreCase))
+             {
+              return kodepos[i];
+              }
+        }
+        return null;
+       
+    }
+}
